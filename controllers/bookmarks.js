4c1f16663e6be92a5e4c2c2912f01b799c098c05
rckmnz <rckmnz05@gmail.com>
2017-08-07 15:26:36 -0400

    test
    test

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -11,6 +11,7 @@ module.exports = (dataLoader) => {
     res.status(500).json({ error: 'not implemented' });
   });
 
+// TEST TEST TEST TEST
 
   // Delete a bookmark
   bookmarksController.delete('/:id', onlyLoggedIn, (req, res) => {
