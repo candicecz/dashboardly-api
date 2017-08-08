beeb2d6a38971625663a8612a965a27e80e8f755
armelchiza <armelchiza@gmail.com>
2017-08-07 20:53:49 -0400

    dsfasdfasf

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -5,13 +5,23 @@ const onlyLoggedIn = require('../lib/only-logged-in');
 module.exports = (dataLoader) => {
   const bookmarksController = express.Router();
 
-  // Modify a bookmark // GIT BRANCH TEST
+  // Modify a bookmark //
   bookmarksController.patch('/:id', onlyLoggedIn, (req, res) => {
     // TODO: this is up to you to implement :)
-    // PATCH makes an update on
+    // PATCH makes an update on bookmarks
+    // dataLoader;
     res.status(500).json({ error: 'not implemented' });
   });
-  
+  // Retrieve a list of boards
+  // boardsController.get('/', (req, res) => {
+  //   dataLoader.getAllBoards({
+  //     page: req.query.page,
+  //     limit: req.query.count
+  //   })
+  //   .then(data => res.json(data))
+  //   .catch(err => res.status(400).json(err));
+  // });
+
   // Delete a bookmark
   bookmarksController.delete('/:id', onlyLoggedIn, (req, res) => {
     // TODO: this is up to you to implement :)
