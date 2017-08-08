1761aa0e0643b32bd1e50a919dddea45951968f2
armelchiza <armelchiza@gmail.com>
2017-08-08 12:06:00 -0400

    asdas

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -6,15 +6,31 @@ module.exports = (dataLoader) => {
   const bookmarksController = express.Router();
 
 
-  // Modify a bookmark //          .onlyLoggedIn,
+  // Modify a bookmark             .onlyLoggedIn,
   bookmarksController.patch('/:id', (req, res) => {
+
+    // req.params.id ==== id of the bookmark
+    // req.body.title
+    // req.body.boardId
+    // req.body.url
+
     // TODO: this is up to you to implement :)
-    // console.log(onlyLoggedIn);
-    return dataLoader.boardBelongsToUser(req.params.id, req.user.id)
-    .then(() => dataLoader.updateBookmark(1, {boardId:3, title:'Gee Biz', URL:'react.oil'}))
-    .then(data => res.json(data))
-    .catch(err => res.status(500).json({ error: 'not implemented' }));
+    // return dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id)
+    // .then(() => dataLoader.updateBookmark(1, {boardId:3, title:'Gee Biz', URL:'react.oil'}))
+    // .then(data => res.json(data))
+    // .catch(err => res.status(500).json({ error: 'not implemented' }));
     // TODO: make sure to verify user
+    // dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id)
+    // .then(() => {
+      return dataLoader.updateBookmark(req.params.id, {
+        title: req.body.title,
+        boardId : req.body.boardId,
+        url: req.body.url
+      })
+    //})
+    .then(data => {console.log(data);
+      return res.json(data)})
+    .catch(err => res.status(400).json(err));
   });
 
 
